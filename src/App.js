ceddef26c85a77866df484243bd196cdfe143404
lurgi <wahoo9040@naver.com>
2023-12-13 15:58:38 +0900

    docs(README): 기능 구현 목록

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 class App {
-  async play() {}
+  async play() {
+    /**
+     * LottoControler 생성
+     * 로또 구입 금액 입력
+     * 구입 금액 만큼 로또 생성
+     * LottoControler에 로또 할당
+     */
+  }
 }
 
 export default App;
