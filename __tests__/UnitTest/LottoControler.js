894892b98553b0c47dfbb0a1a9003d09d1814c70
lurgi <wahoo9040@naver.com>
2023-12-13 19:47:20 +0900

    test(LottoControler): 보너스 번호 입력 테스트

--- __tests__/UnitTest/LottoControler.js
+++ __tests__/UnitTest/LottoControler.js
@@ -17,4 +17,10 @@ describe('LottoControler 테스트', () => {
       expect(() => LOTTO_CONTROLER.setWinNumbers(numbers)).toThrow('[ERROR]');
     },
   );
+
+  test.each([NaN, 0, 46, 4])('보너스 번호 예외 처리 테스트', (number) => {
+    const LOTTO_CONTROLER = new LottoControler();
+    LOTTO_CONTROLER.setWinNumbers('1,2,3,4,5,6');
+    expect(() => LOTTO_CONTROLER.setBonusNumber(number)).toThrow('[ERROR]');
+  });
 });
