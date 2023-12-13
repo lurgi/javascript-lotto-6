142429b76609925a54f6956d024b7b015bd232be
lurgi <wahoo9040@naver.com>
2023-12-13 16:26:20 +0900

    feat(App): 로또 구입 구현

--- src/App.js
+++ src/App.js
@@ -12,12 +12,6 @@ class App {
   }
 
   async playAmountMoney() {
-    /**
-     * LottoControler 생성
-     * 로또 구입 금액 입력
-     * 구입 금액 만큼 로또 생성
-     * LottoControler에 로또 할당
-     */
     try {
       const AMOUNT_MONEY = await InputView.readAmountMoney();
       this.#lottoControler.createLottos(AMOUNT_MONEY);
--- src/model/LottoControler.js
+++ src/model/LottoControler.js
@@ -1,15 +1,33 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
+
+const CONSTANT = Object.freeze({
+  lottoPrice: 1_000,
+});
+
 class LottoControler {
+  #lottos;
+
   createLottos(money) {
     this.#moneyValid(money);
+    const LOTTO_CNT = money / CONSTANT.lottoPrice;
+    this.#lottos = Array.from({ length: LOTTO_CNT }, () => {
+      const RANDOM_NUMBERS = this.#randomeSixNumber();
+      return new Lotto(RANDOM_NUMBERS);
+    });
   }
 
   #moneyValid(money) {
-    if (money < 1_000) {
+    if (money < CONSTANT.lottoPrice) {
       throw new Error('[ERROR]');
     }
-    if (money % 1_000 !== 0) {
+    if (money % CONSTANT.lottoPrice !== 0) {
       throw new Error('[ERROR]');
     }
+  }
+
+  #randomeSixNumber() {
+    MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 }
 
