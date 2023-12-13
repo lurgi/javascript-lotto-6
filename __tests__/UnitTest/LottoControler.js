cd3b36551bf4c32410d6414764c97f1b84417feb
lurgi <wahoo9040@naver.com>
2023-12-13 18:38:49 +0900

    test(LottoControler): 당첨 번호 입력 테스트

--- __tests__/UnitTest/LottoControler.js
+++ __tests__/UnitTest/LottoControler.js
@@ -1,11 +1,20 @@
 import LottoControler from '../../src/model/LottoControler';
 
+/* eslint-disable */
 describe('LottoControler 테스트', () => {
   test.each([500, 1500, 1233, 1111111, 0])(
     '구매 금액 예외 처리 테스트',
     (money) => {
       const LOTTO_CONTROLER = new LottoControler();
       expect(() => LOTTO_CONTROLER.createLottos(money)).toThrow('[ERROR]');
+    },
+  );
+
+  test.each(['1,2,3,4,4,4', 'q,eqreakj,erjla', '123,4,5,6,7,8'])(
+    '우승 번호 예외 처리 테스트',
+    (numbers) => {
+      const LOTTO_CONTROLER = new LottoControler();
+      expect(() => LOTTO_CONTROLER.setWinNumbers(numbers)).toThrow('[ERROR]');
     },
   );
 });
